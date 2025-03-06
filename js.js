38d7bc96e8cc76778d2eb7e4f80754a54abf0bb8
Peterblf <pierrebrech@icloud.com>
2025-03-06 15:08:32 +0100

    Delete js.js

--- js.js
+++
@@ -1,8 +0,0 @@
-        document.getElementById("valider").addEventListener("click", function() {
-            let reponse = document.getElementById("reponse").value.toLowerCase();
-            if (reponse === "Saloon" || reponse === "saloon" || reponse === "sale loup nœud") { // Remplace "réponse_attendue" par la bonne réponse
-                window.location.href = "qr_code.html";
-            } else {
-                alert("Mauvaise réponse, essayez encore !");
-            }
-        });
